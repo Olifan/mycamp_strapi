78ddde460e3d10c02672ad68c6add4037f6ccc55
Maksym Charuk <maxcharuk@gmail.com>
2022-10-16 15:28:03 -0400

    Add Cloudinary

--- src/api/palaroid-photo/services/palaroid-photo.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * palaroid-photo service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::palaroid-photo.palaroid-photo');
