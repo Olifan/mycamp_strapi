e49d53e2574b4d1ac5eaabe6479a33466658e4b7
Olifan <olifananton@gmail.com>
2023-02-22 23:24:00 +0200

    Shift tabs

---
+++ src/api/camp-program/services/camp-program.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * camp-program service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::camp-program.camp-program');
