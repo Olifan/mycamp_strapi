a200fd18946b156aff8a02063b11c3ef25618b44
Olifan <olifananton@gmail.com>
2023-03-12 23:50:52 +0200

    Add photo-album and photo-page

---
+++ src/api/photo-page/routes/photo-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * photo-page router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::photo-page.photo-page');
---
+++ src/api/photo-page/services/photo-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * photo-page service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::photo-page.photo-page');
